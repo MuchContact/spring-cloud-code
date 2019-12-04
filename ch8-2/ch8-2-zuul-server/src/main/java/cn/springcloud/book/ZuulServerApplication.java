0e1334132f435cc66b18abea9012e18e37a970f2
MuchContact <422549477@qq.com>
2019-12-04 15:14:37 +0800

    添加actuator

--- ch8-2/ch8-2-zuul-server/src/main/java/cn/springcloud/book/ZuulServerApplication.java
+++ ch8-2/ch8-2-zuul-server/src/main/java/cn/springcloud/book/ZuulServerApplication.java
@@ -7,10 +7,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
+//@EnableOAuth2Client
 @EnableOAuth2Sso
 public class ZuulServerApplication extends WebSecurityConfigurerAdapter{
 
@@ -22,7 +24,7 @@ public class ZuulServerApplication extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-		.antMatchers("/login", "/client/**")
+		.antMatchers("/login", "/client/**", "/actuator/**")
 		.permitAll()
 		.anyRequest()
 		.authenticated()
